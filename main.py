e1f6357f43d901b8082f49f877ec0b9c1eda1538
IsaacUgalino <isugalino@s.sfusd.edu>
2025-04-16 10:02:05 -0700

    c2

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pygame
+import time
+import sys
 
-pygame.init() #initialize program
+background_colour = (255,255,255)
 (width, height) = (500, 500)
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Asteroids")
+screen.fill(background_colour)
+
 pygame.display.flip()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+class Character:
+    def _init_(self, image, x, y):
+        self.image = pygame.image.load(image)
+        self.x = x
+        self.y = y
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+    
