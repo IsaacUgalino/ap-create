26cebd2b5c152262a7224e5562f8a271e3046416
IsaacUgalino <isugalino@s.sfusd.edu>
2025-04-21 19:52:44 -0700

    c3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import sys
 
-background_colour = (255,255,255)
+background_colour = (0,0,0)
 (width, height) = (500, 500)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Asteroids")
@@ -10,18 +10,27 @@ screen.fill(background_colour)
 
 pygame.display.flip()
 
+#player icon
+player_image = pygame.image.load("spaceship.png").convert_alpha() #convert alpha makes image transparent 
+player_rect = player_image.get_rect() #gets rectangle area of image
+player_rect.center = (width // 2, height // 2) #centers box on screen
+player_speed = 6
+
+clock = pygame.time.Clock()
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-class Character:
-    def _init_(self, image, x, y):
-        self.image = pygame.image.load(image)
-        self.x = x
-        self.y = y
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
-
-    
+    #Key controls
+    keys = pygame.get_pressed()
+    if keys[pygame.K_w]: #move up
+        player_rect.w -= player_speed
+    if keys[pygame.K_s]:
+        [player_rect.s] += player_speed
+    if keys[pygame.K_a]:
+        player_rect.a -= player_speed
+    if keys[pygame.K_d]:
+        player_rect.d += player_speed
